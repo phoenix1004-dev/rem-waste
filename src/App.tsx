8d85964d504565def35a841efdeea753b4f0337b
phoenix1004-dev <181405513+phoenix1004-dev@users.noreply.github.com>
2025-05-09 18:14:13 +0700

    feat: put the title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Homepage from "./pages/Homepage";
 
+import './App.css';
+
 export default function App() {
 
   return (
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,3 +1,8 @@
 export default function Homepage() {
-    return <></>
+    return (
+        <div className="w-full h-full max-w-7xl py-12">
+            <h1 className="text-5xl font-bold text-center mb-6 text-white">Choose Your Skip Size</h1>
+            <p className="text-xl text-center text-gray-300">Select the skip size that best suits your needs</p>
+        </div>
+    )
 }
