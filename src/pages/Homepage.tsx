0f2f2255545580a25881ca08ee0a3771b8abef60
phoenix1004-dev <181405513+phoenix1004-dev@users.noreply.github.com>
2025-05-10 22:33:21 +0700

    feat: add selected item detail

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -11,6 +11,9 @@ export default function Homepage() {
   const [error, setError] = useState<string | null>(null);
   const [selectedSkipId, setSelectedSkipId] = useState<number | null>(null);
 
+  // Find the selected skip from the skips array
+  const selectedSkip = skips?.find((skip) => skip.id === selectedSkipId);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -34,7 +37,7 @@ export default function Homepage() {
   };
 
   return (
-    <div className="w-full h-full max-w-7xl px-4 py-12">
+    <div className="w-full h-full max-w-7xl px-4 py-12 pb-20">
       <h1 className="text-5xl font-bold text-center mb-6 text-white">
         Choose Your Skip Size
       </h1>
@@ -61,6 +64,33 @@ export default function Homepage() {
               isSelected={skip.id === selectedSkipId}
             />
           ))}
+        </div>
+      )}
+
+      {/* Fixed position selected skip bar */}
+      {selectedSkip && (
+        <div className="fixed bottom-0 left-0 right-0 bg-black border-t border-gray-500 py-4 px-6 justify-items-center">
+          <div className="w-full max-w-7xl md:flex md:justify-between md:items-center">
+            <div className="flex items-center gap-4 justify-center md:justify-start">
+              <span className="text-white font-bold">
+                {selectedSkip.size} Yard Skip
+              </span>
+              <span className="text-blue-600 font-bold text-xl">
+                £{selectedSkip.price_before_vat}
+              </span>
+              <span className="text-gray-400">
+                {selectedSkip.hire_period_days} day hire
+              </span>
+            </div>
+            <div className="grid grid-cols-2 md:flex items-center gap-4 mt-4 md:mt-0">
+              <button className="text-white bg-[#666666] rounded-lg hover:bg-[#888888] px-6 py-2">
+                Back
+              </button>
+              <button className="bg-blue-700 hover:bg-blue-600 text-white px-6 py-2 rounded-md flex items-center justify-center">
+                Continue <span className="ml-1">→</span>
+              </button>
+            </div>
+          </div>
         </div>
       )}
     </div>
